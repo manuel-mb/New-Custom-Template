04eb331836b8e258788f61a665b488e598cb6536
manuel-mb <85565518+manuel-mb@users.noreply.github.com>
2022-02-08 16:47:13 +0000

    Update

--- index.php
+++ index.php
@@ -15,33 +15,29 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="content" class="main">
 
 		<?php
-		if ( have_posts() ) :
+		if ( have_posts() ) : ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+
+			<section class="main__content">
 				<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
+			while ( have_posts() ) {
 				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+				}
+				?>
+			</section>
 
-			the_posts_navigation();
+			<?php
+			the_posts_navigation( [
+				'prev_text' => __( 'Prev', 'Manuel' ),
+				'next_text' => __( 'Next', 'Manuel' ),
+			] );
 
 		else :
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,63 +1,18 @@
 <?php
 /**
- * Template part for displaying posts
+ * The default template part for post content in the loop.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
+ * 
  * @package Manuel
  */
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				manuel_posted_on();
-				manuel_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+<article <?php post_class( [ 'content', 'content__post' ] ); ?>>
+	<h2 class="content__title"><?php the_title(); ?></h2>
 
-	<?php manuel_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'manuel' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'manuel' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php manuel_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	<div class="content_body">
+		<?php the_content(); ?>
+	</div>
+</article>
