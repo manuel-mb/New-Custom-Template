ad7d8aa7b85929ea8bd4ddf4916ac743d4dcc4fc
manuel-mb <manuel@miltonbayer.com>
2022-02-22 16:14:34 +0000

    Update

--- template-parts/header-branding.php
+++ template-parts/header-branding.php
@@ -1,4 +1,4 @@
-<div class="header__site-branding">
+<div class="header-site-branding">
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-branding__link" rel="home" itemprop="url">
 		<?php get_template_part( 'template-parts/site-logo' ); ?>
 	</a>
